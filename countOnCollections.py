fdfb22d3bebebb4984c366b17af47384ee8c33c3
David Goraj <dg@netkitchen.de>
2024-06-04 15:43:03 +0200

    count on collections like lists or tuples with special value or lowest/highest

---
+++ countOnCollections.py
@@ -0,0 +1,15 @@
+def countOnCollectionWithVal(collection, value = "max"):
+    if isinstance(value, str):
+        if value == "max" or value == "high":
+            value = max(collection)
+        elif value == "min" or value == "low":
+            value = min(collection)
+
+    return collection.count(value)
+
+print("7: ", countOnCollectionWithVal((7,7,7,7,2,5,3), 7))
+print("8: ", countOnCollectionWithVal([7,7,7,7,2,5,3], 8))
+print("leer: ", countOnCollectionWithVal([7,7,7,7,2,5,3]))
+print("min: ", countOnCollectionWithVal([7,7,7,7,2,5,3], 'min'))
+print("low: ", countOnCollectionWithVal([7,7,7,7,2,5,3], 'low'))
+print("high: ", countOnCollectionWithVal([7,7,7,7,2,5,3], 'high'))
