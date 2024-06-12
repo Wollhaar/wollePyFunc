4660d7dd7fbafed52fa30286d730cd7a0d44f23c
David Goraj <dg@netkitchen.de>
2024-06-12 13:25:31 +0200

    workin with lamda func in a func

--- lambdaList.py
+++ lambdaList.py
@@ -1,3 +1,12 @@
 greet = lambda name: "Welcome, " + name
 
 print(greet("Bob"))
+
+def mult(n):
+  return lambda a : a * n
+
+doubler = mult(2)
+tripler = mult(3)
+
+print(doubler(5))
+print(tripler(5))
