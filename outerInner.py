cb67fb7471ee5008a91686437d8832673309a0cc
David Goraj <dg@netkitchen.de>
2024-06-13 18:46:41 +0200

    decorating another function with a wrapper function

--- outerInner.py
+++ outerInner.py
@@ -8,3 +8,20 @@ def greet(name):
     return message
 
 print(greet("Bob"))
+
+
+def greet():
+    return "Welcome!"
+
+#takes a function as an argument
+def uppercase(func):
+    #wrapper function to keep the
+    #original function code unchanged
+    def wrapper():
+        orig_message = func()
+        modified_message = orig_message.upper()
+        return modified_message
+    return wrapper
+
+greet_upper = uppercase(greet)
+print(greet_upper())
