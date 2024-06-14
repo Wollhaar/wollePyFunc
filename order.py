356506dfd7d5f9400f31fd35024abe3077229c6e
David Goraj <dg@netkitchen.de>
2024-06-14 18:52:40 +0200

    import Car to order, instantiate a car and turn object to string

--- order.py
+++ order.py
@@ -1,4 +1,6 @@
 import funcCtrl
+from OOP.Car import Car
+
 
 def process_order(dish, func):
     print(func(dish))
@@ -7,3 +9,8 @@ process_order("Chilli sin carne", funcCtrl.order)
 process_order("Chilli sin carne", funcCtrl.prepared)
 process_order("Chilli sin carne", funcCtrl.cooked)
 process_order("Chilli sin carne", funcCtrl.ready)
+
+car1 = Car('Ford', 'Mustang', 'black')
+process_order(str(car1), funcCtrl.order)
+process_order("Car " + str(car1), funcCtrl.prepared)
+process_order("Car " + str(car1), funcCtrl.ready)
